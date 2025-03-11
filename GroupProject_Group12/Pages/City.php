1230562f88a11f816284075c4372a2fbe3cb1178
Phillip Elwis <Pelwis01@gmail.com>
2025-03-11 15:29:49 +0000

    Added Chart.js link back

--- GroupProject_Group12/Pages/City.php
+++ GroupProject_Group12/Pages/City.php
@@ -5,7 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <link rel="stylesheet" href="/Group_Project/GroupProject_Group12/style.css">
-    
+
+    <!-- chart.js -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> 
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
--- GroupProject_Group12/Pages/Login.php
+++ GroupProject_Group12/Pages/Login.php
@@ -6,6 +6,9 @@
    
     <link rel="stylesheet" href="/Group_Project/GroupProject_Group12/style.css">
     <link rel="stylesheet" href="/Group_Project/GroupProject_Group12/Loginstyle.css">
+
+    <!-- chart.js -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
