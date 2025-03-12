a66378ec9e5e47a69c7693d7a1fc46d69c19c6d3
Phillip Elwis <Pelwis01@gmail.com>
2025-03-12 16:05:21 +0000

    Pre log update

--- GroupProject_Group12/Pages/Admin.php
+++ GroupProject_Group12/Pages/Admin.php
@@ -30,59 +30,62 @@
         <div id = "AdminHeading">
             <h2>Admin Page:</h2>
         </div>
-        
-        <div class="panel-group">
-        
-            <div  id = "AdminPanel" class="panel panel-default">
-                <div class="panel-heading" id = "AdminPanelHeading">Create New User:</div>
-                <div class="panel-body" id = "AdminPanelBody">
-                    
-                    <form id = "AdminPanelForm" action = "#">
-                        <div id = "AdminPanelFormContent">
-                            <div id = "AdminPanelFormContentInputs">
-                                <label for = "Email">Email Address:</label>
-                                <input type="text" id = "email"  name = "Email">
-                            </div>
-                            <div id = "AdminPanelFormContentInputs">
-                                <label for = "Password">Password:</label>
-                                <input type="password" id = "Password"  name = "Password">
-                            </div>
-                            <div id = "AdminPanelFormContentInputs" style = "margin-bottom: 25px;">
-                                <label for = "ConPass">Confirm Password:</label>
-                                <input type="password" id = "ConPass"  name = "ConPass">
-                            </div>
-                        </div>
-                        
-                        <div id = "AdminPanelFormRigth">
-                            <div style = "margin-top: 10px">
-                                <input class = "form-check-input" type = "radio" id = "Network User" value = "Network User">
-                                <label class = "form-check-label" for = "Network User">Network User </label> 
+    
+
+        <div class="row">
+            <!-- Big chart panel (fat) -->
+            <div class="col-12  col-md-4">
+                <div class="card" style="height:370px;">
+                    <div class="card-header">📊 Create a New User:</div>
+                    <div class="card-body" >
+                       
+                        <form id = "AdminPanelForm" action = "#">
+                            <div id = "AdminPanelFormContent">
+                                <div id = "AdminPanelFormContentInputs">
+                                    <label for = "Email">Email Address:</label><br>
+                                    <input type="text" id = "email"  name = "Email">
+                                </div>
+                                <div id = "AdminPanelFormContentInputs">
+                                    <label for = "Password">Password:</label><br>
+                                    <input type="password" id = "Password"  name = "Password">
+                                </div>
+                                <div id = "AdminPanelFormContentInputs" style = "margin-bottom: 25px;">
+                                    <label for = "ConPass">Confirm Password:</label><br>
+                                    <input type="password" id = "ConPass"  name = "ConPass">
+                                </div>
                             </div>
                             
-                            <div>
-                                <input class = "form-check-input" type = "radio" id = "City Council User" value = "City Council User"> 
-                                <label class = "form-check-label" for = "City Council User">City Council User </label> 
-                            </div> 
-                            
-                            <div id = "AdminPanelAddUserBtn">
-                                <button class = "btn btn-light"  type = "Submit">Add User</button> 
+                            <div id = "AdminPanelFormRigth">
+                                <div style = "margin-top: 10px">
+                                    <input class = "form-check-input" type = "radio" id = "Network User" value = "Network User">
+                                    <label class = "form-check-label" for = "Network User">Network User </label> 
+                                </div>
+                                
+                                <div>
+                                    <input class = "form-check-input" type = "radio" id = "City Council User" value = "City Council User"> 
+                                    <label class = "form-check-label" for = "City Council User">City Council User </label> 
+                                </div> 
+                                
+                                <div id = "AdminPanelAddUserBtn">
+                                    <button class = "btn btn-light"  type = "Submit">Add User</button> 
+                                </div>
                             </div>
-                        </div>
-                        
-                    </form>
+                            
+                        </form>  
+                    </div>
                 </div>
-                
-            </div>
-        
-            <!-- Second Panel -->
-            <div style = "position: absolute; bottom: 345px; right: 0; height: 325px; margin-top: 25px; text-align: center; color: white; width: 45%; border: 1px solid; border-radius: 16px;" class="panel panel-default">
-                <div class="panel-heading" style = "text-decoration-line: underline">Delete User:</div>
-                <div class="panel-body"></div>
             </div>
 
         </div>
         
     </div>
-    
+<script>
+    document.getElementByID    
+</script>
 </body>
+
+
+
+
+
 </html>
