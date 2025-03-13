732da92dd44f17e599fec1a9a851982578aff652
Elwis <c3031863@hallam.shu.ac.uk>
2025-03-13 11:12:27 +0000

    Login page style fix

--- GroupProject_Group12/Pages/Login.php
+++ GroupProject_Group12/Pages/Login.php
@@ -3,10 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <script src="/Group_Project/GroupProject_Group12/scripts/login_modal.js"></script>
 
-    <link rel="stylesheet" href="/Group_Project/GroupProject_Group12/login.css">
+    <link rel="stylesheet" href="/Group_Project/GroupProject_Group12/styles/login.css">
 
     <title>Login - Smart Energy Dashboard</title>
 </head>
