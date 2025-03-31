7fbbefff5ab7ffb5d8dbdf49f60ee418601843a7
Phillip Elwis <Pelwis01@gmail.com>
2025-03-31 19:39:30 +0100

    Got rid of create admin bar

--- GroupProject_Group12/modules/createuser.php
+++ GroupProject_Group12/modules/createuser.php
@@ -35,10 +35,6 @@
                             <input class="form-check-input" type="checkbox" id="City_Council_User" name="City_Council_User" onChange="UserType()"> 
                             <label class="form-check-label" for="City_Council_User">City council user</label> 
                         </div> 
-                        <div style="margin-top: 10px">
-                            <input class="form-check-input" type="checkbox" id="AdminUser" name="Admin_User" > 
-                            <label class="form-check-label" for="AdminUser">Admin user</label> 
-                        </div> 
                             <!-- Network and city select statements -->
                         <div id="Network_Select" style="display: none;" class="themed-dropdown">
                             <label for="Networks">Select network:</label> <br>
