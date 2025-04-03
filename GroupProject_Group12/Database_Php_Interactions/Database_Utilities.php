4df7b783bf064cd67436c988b5c6e3db2368c237
Benjamin Ryder <1benryderz@gmail.com>
2025-04-03 08:29:53 +0100

    cleaned up utility files

--- GroupProject_Group12/Database_Php_Interactions/Database_Utilities.php
+++ GroupProject_Group12/Database_Php_Interactions/Database_Utilities.php
@@ -1,22 +1,22 @@
-<?php  
-    //Function to Write data into the console for error checking
+<?php 
+    // Function to Write data into the console for error checking
     function debug_to_console($data) {
-        //Assigns data given into local variable 
+        // Assigns data given into local variable
         $output = $data;
-        //Checks if variable is an array then implods (returns string of variable in array) and then Echos the problem into console
+        // Checks if variable is an array then implods (returns string of variable in array) and then Echos the problem into console
         if (is_array($output))
-            $output = implode(',', $output);                        
+            $output = implode(',', $output);
         echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
     }
-
-    //Function to open the data base *Note does not close connection*
+    
+    // Function to open the data base *Note does not close connection*
     function Open_Database() {
-        //Tries to open database if it fails it will Debug to console for why
+        // Tries to open database if it fails it will Debug to console for why
         try  { 
-            //Assigns database path to variable to be used later and creates connection
+            // Assigns database path to variable to be used later and creates connection
             $db = new SQLite3('../database/users.db');
             
-            //If connection is made console log Database connection success else it shows An error on why
+            // If connection is made console log Database connection success else it shows An error on why
             if ($db) {
                 echo "<script> console.log('Database Opened Successfully') </script>";
             } else {
@@ -24,9 +24,9 @@
                 exit;
             }
         } catch (Exception $e) {
-            debug_to_console(  $e->getMessage());
+            debug_to_console($e->getMessage());
         }
-        //Returns Database connection to use for queries
+        // Returns Database connection to use for queries
         return $db;
     }
 ?>
--- GroupProject_Group12/modules/createuser.php
+++ GroupProject_Group12/modules/createuser.php
@@ -1,18 +1,18 @@
-<!-- 🔒 Revamped Login Modal -->
+<!-- 🔐 Create user modal -->
 <div class="modal fade" id="CreateModal" tabindex="-1" aria-labelledby="CreateModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered"> <!-- Centered Modal -->
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h2>Create User</h2>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-
-            <!-- 📝 Login Form -->
+            
+            <!-- 📝 Create user form -->
             <div class="modal-body">
                 <form id="AdminPanelForm" action="../Database_Php_Interactions/Create_New_User.php" method="POST">
                     <div id="AdminPanelFormContent">
                         <div id="AdminPanelFormContentInputs">
-                            <label for="Email">Email Address:</label><br>
+                            <label for="Email">Email address:</label><br>
                             <input type="email" id="Email" name="Email" placeholder="example@email.com">
                         </div>
                         <div id="AdminPanelFormContentInputs">
@@ -20,11 +20,11 @@
                             <input type="password" id="Password" name="Password">
                         </div>
                         <div id="AdminPanelFormContentInputs" style="margin-bottom: 25px;">
-                            <label for="ConPass">Confirm Password:</label><br>
+                            <label for="ConPass">Confirm password:</label><br>
                             <input type="password" id="ConPass" name="ConPass">
                         </div>
                     </div>
-                    <!-- Network and city assignment code -->
+                    <!-- 🔗 Network and city assignment code -->
                     <div id="AdminPanelFormRight">
                         <div style="margin-top: 10px">
                             <input class="form-check-input" type="checkbox" id="Network_User" name="Network_User" onChange="UserType()">
@@ -35,7 +35,7 @@
                             <input class="form-check-input" type="checkbox" id="City_Council_User" name="City_Council_User" onChange="UserType()"> 
                             <label class="form-check-label" for="City_Council_User">City council user</label> 
                         </div> 
-                        <!-- Network and city select statements -->
+                        <!-- 📋 Network and city select statements -->
                         <div id="Network_Select" style="display: none;" class="themed-dropdown">
                             <label for="Networks">Select network:</label> <br>
                             <select class = "form-select" name="Networks" id="Networks">
@@ -47,30 +47,30 @@
                                 <option value="Stedin"> Stedin </option>
                                 <option value="Liander"> Liander </option>
                             </select>
-                        </div> 
-
+                        </div>
+                        
                         <div id="City_Select" style="display: none;" class="themed-dropdown">
                             <label for="Cities">Select city:</label> <br>
                             <select class="form-select" name="Cities" id="Cities">
                                 <?php include("../Database_Php_Interactions/CitySelect.php");?>
                             </select>
-                        </div> 
+                        </div>
 
                         <?php 
                             if (isset($_GET['CreateUser'])) {
                                 if( $_GET['CreateUser']) {
-                                    echo"<div Style = 'color: Green'> Account Successfully Created </div>";
+                                    echo"<div style='color: green'> Account Successfully Created </div>";
                                 } else {
-                                    echo"<div Style = 'color: Red'> Account Creation Failed </div>";
+                                    echo"<div style='color: red'> Account Creation Failed </div>";
                                 }
-                            }  
+                            }
                         ?>
                         <div id="AdminPanelAddUserBtn">
                             <button type="submit" class="fancy-button" style="float: right">Add User</button> 
                         </div>
                     </div>
                     
-                </form> 
+                </form>
             </div>
         </div>
     </div>
