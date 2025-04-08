3eb91bab7ecb07c138ae2431fb68d35a1983f1f3
Benjamin Ryder <1benryderz@gmail.com>
2025-04-08 13:49:05 +0100

    fixed notifications (mostly)
    more work to be done here!!!

--- GroupProject_Group12/Pages/notifications.php
+++ GroupProject_Group12/Pages/notifications.php
@@ -21,7 +21,7 @@
             <h2> Notifications </h2>
         </div>
         
-        <div class="notification-list">
+        <div class="row gx-1">
             <?php
                 // 🔗 Connect to the database
                 $conn = Open_Database();
@@ -32,14 +32,20 @@
                 
                 // 🔄 Loop through the notifications and display them
                 while ($notification = $notifResult->fetchArray(SQLITE3_ASSOC)) {
-                    echo '<div class="notification-item">';
-                    echo '<p class="notification-text">' . htmlspecialchars($notification['Body']) . '</p>';
-                    // 🗑️ Delete
-                    echo '<form method="POST" action="" style="display:inline;">
-                            <input type="hidden" name="NotifID" value="' . $notification['NotifID'] . '" />
-                            <button type="submit" name="deleteNotification" class="btn btn-danger delete-button">Delete</button>
-                          </form>';
-                    echo '</div>';
+                    echo '
+                        <div class="card mb-3 d-flex">
+                            <form method="POST" action="" style="display:inline;">
+                                <div class="card-header">
+                                    Notification
+                                    <button type="submit" name="deleteNotification" class="btn-close" data-bs-dismiss="modal" aria-label="Close" style="position: absolute; right: 12px"></button>
+                                </div>
+                                <div class="card-body">
+                                    <p class="notification-text">' . htmlspecialchars($notification['Body'] ?? '') . '</p>
+                                    <input type="hidden" name="NotifID" value="' . htmlspecialchars($notification['NotifID'] ?? '') . '"/>
+                                </div>
+                            </form>
+                        </div>
+                    ';
                 }
             ?>
         </div>
